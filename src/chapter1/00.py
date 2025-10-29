96001c7b7fbe41f9d209e478672ec26d16d9536b
KITAHARA Kota <110091534+Naiseki@users.noreply.github.com>
2025-10-29 23:24:17 +0900

    00.py追加

---
+++ src/chapter1/00.py
@@ -0,0 +1,11 @@
+def main():
+    s1 = "パトカー"
+    s2 = "タクシー"
+    result = ""
+    for i in range(len(s1)):
+        result += s1[i] + s2[i]
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
