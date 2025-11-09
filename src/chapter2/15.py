46e420abecd75ecb0f2ec9bcc7a6cb15cb7abfea
kitahara <kitahara@ai.cs.ehime-u.ac.jp>
2025-11-09 22:53:26 +0900

    15.py (ファイルをN分割する) 追加

---
+++ src/chapter2/15.py
@@ -0,0 +1,21 @@
+import sys
+import numpy as np
+
+def split_file(file, n):
+    lines = file.readlines()
+    return np.array_split(lines, n)
+
+def main():
+    filename = sys.argv[1]
+    n = int(sys.argv[2])
+    parts = []
+    with open(filename, 'r') as file:
+        parts = split_file(file, n)
+    for i in range(len(parts)):
+        with open(f'{filename}_{i}.txt', 'w') as out_file:
+            out_file.writelines(parts[i])
+            
+
+
+if __name__ == '__main__':
+    main()
