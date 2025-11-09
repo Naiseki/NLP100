c86564f365ff802a8fce3859300dcecc3ff6c59c
kitahara <kitahara@ai.cs.ehime-u.ac.jp>
2025-11-09 22:05:54 +0900

    11.py追加

---
+++ src/chapter2/11.py
@@ -0,0 +1,19 @@
+import sys
+
+def print_head(file, n):
+    for i in range(n):
+        line = file.readline()
+        if line is None:
+            break
+        print(line)
+
+
+def main():
+    filename = sys.argv[1]
+    n = int(sys.argv[2])
+    with open(filename, "r") as file:
+        print_head(file, n)
+
+
+if __name__ == "__main__":
+    main()
