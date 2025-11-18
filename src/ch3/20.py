4fd4d39495992fd3af5871a5cef6cbfb8dda0e13
kitahara <kitahara@ai.cs.ehime-u.ac.jp>
2025-11-18 18:23:38 +0900

    20追加

---
+++ src/ch3/20.py
@@ -0,0 +1,24 @@
+import sys
+import gzip
+import json
+
+def find_title(stream, target_title):
+    for line in stream:
+        article_dict = json.loads(line)
+        if target_title in article_dict["title"]:
+            return article_dict
+    return None
+
+def main():
+    path = "input/jawiki-country.json.gz"
+    with gzip.open(path, "rb") as file:
+        target_title = "イギリス"
+        article = find_title(file, target_title)
+        if article:
+            print(article["text"])
+        else:
+            print(f"'{target_title}'の記事は見つかりませんでした。")
+
+
+if __name__ == "__main__":
+    main()
