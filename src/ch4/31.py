210cc1df7bde0b14fcfae43dd1063f47d93b362e
kitahara <kitahara@ai.cs.ehime-u.ac.jp>
2025-11-25 14:50:42 +0900

    31追加

---
+++ src/ch4/31.py
@@ -0,0 +1,26 @@
+import sys
+import gzip
+import json
+from sudachipy import dictionary, tokenizer
+from collections import Counter
+
+def main():
+    text = """
+    メロスは激怒した。
+    必ず、かの邪智暴虐の王を除かなければならぬと決意した。
+    メロスには政治がわからぬ。
+    メロスは、村の牧人である。
+    笛を吹き、羊と遊んで暮して来た。
+    けれども邪悪に対しては、人一倍に敏感であった。
+    """
+
+    tokenizer_obj = dictionary.Dictionary().create()
+
+    tokens = tokenizer_obj.tokenize(text, tokenizer.Tokenizer.SplitMode.C)
+    for t in tokens:
+        pos = t.part_of_speech()
+        print(pos[0], t.dictionary_form())
+
+
+if __name__ == "__main__":
+    main()
